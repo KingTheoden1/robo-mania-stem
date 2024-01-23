82519f3242e0a1fab8d0c513b70128365c624832
Isaac <isaacfunes@icloud.com>
2024-01-23 13:16:02 -0600

    Created a Why Us area

--- components/brand/Brand.jsx
+++ components/brand/Brand.jsx
@@ -5,7 +5,11 @@ import './brand.css';
 const Brand = () => {
   return (
     <div className="robomania__brand section__padding">
-      <h2 className="robomania__brand-title">Our Partners and Clients</h2> {/* Title added here */}
+      <div className="why-us-content">
+        <h2 className='gradient__text'>Why Us</h2>
+        <p>Here you can explain why customers should choose your service or product, highlighting your unique selling points.</p>
+      </div>
+      <h2 className="gradient__text robomania__brand-title">Our Partners and Clients</h2> {/* Title added here */}
       <div className="robomania__brand-row">
         <img src={tamu} alt="tamu" />
         <img src={agrilife} alt="agrilife" />
